bac4de6940d8fd7d4552b761e4ec484333572209
Jessica <1330784336@qq.com>
2021-12-04 15:33:46 +0800

    更新DOM库

---
+++ dist/dom.be574493.js
@@ -0,0 +1,466 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"dom.js":[function(require,module,exports) {
+window.dom = {
+    //创建元素：首先定义一个container容器放创建的新标签，template可以放入任意元素，
+    //利用innerHTML将容器中的内容变成string，trim（）的作用是去掉字符串两侧的空格
+    //最后返回firstChild。
+    create: function create(string) {
+        var container = document.createElement("template");
+        container.innerHTML = string.trim();
+        return container.content.firstChild;
+    },
+
+    // 插入后一个兄弟元素：插入node下一个节点的前面，相当于插入node的后面
+    after: function after(node, didi) {
+        node.parentNode.insertBefore(didi, node.nextSiling);
+    },
+
+    //插入一个哥哥元素
+    before: function before(node, gege) {
+        node.parentNode.insertBefore(gege, node);
+    },
+
+    //新增一个子元素
+    append: function append(parent, node) {
+        parent.appendChild(node);
+    },
+
+    //新增一个父元素：把父元素插入到子元素之前，再将子元素插入到父元素中，原来的子元素的位置就会移开
+    wrap: function wrap(node, parent) {
+        dom.before(node, parent);
+        dom.append(parent, node);
+    },
+
+    //删除元素
+    remove: function remove(node) {
+        node.parentNode.removeChild(node);
+        return node;
+    },
+
+    //删除所有子元素:用while判断node的第一个子节点是否存在，存在就执行删除操作，并把删除的值放到数组中
+    //当x不存在即node子元素删除完结束循环，返还被删除的元素的数组。
+    empty: function empty(node) {
+        var childNodes = node.childNodes;
+        var array = [];
+        var x = node.firstChild;
+        while (x) {
+            array.push(dom.remove(x));
+            x = node.firstChild;
+        }
+        return array;
+    },
+
+    //修改或查看元素的属性值：当参数的长度为3时，为修改node属性值
+    //当参数的长度为2时，为查看node中的某一属性值
+    attr: function attr(node, name, value) {
+        if (arguments.length === 3) {
+            node.setAttribute(name, value);
+        } else if (arguments.length === 2) {
+            return node.setAttribute(name);
+        }
+    },
+
+    //读写文本内容
+    text: function text(node, string) {
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string;
+            } else {
+                node.textContent = string;
+            }
+        } else if (arguments.length === 1) {
+            if ('innerText' in node) {
+                return node.innerText;
+            } else {
+                return node.textContent;
+            }
+        }
+    },
+
+    //读写html内容
+    html: function html(node, string) {
+        if (arguments.length === 2) {
+            node.innerHTML = string;
+        } else if (arguments.length === 1) {
+            return node.innerHTML;
+        }
+    },
+
+    //读取或修改style属性
+    style: function style(node, name, value) {
+        //style(text,'color','red'),当长度为3时修改name的value值
+        if (arguments.length === 3) {
+            node.style[name] = value;
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                //style(test,'color')，长度为2且name类型为string时查看name属性值
+                return node.style[name];
+            } else if (name instanceof Object) {
+                //style(test,{color:'red'})长度为2且name为对象时修改name的style
+                for (var key in name) {
+                    node.style[key] = name[key];
+                }
+            }
+        }
+    },
+
+    //添加class
+    class: {
+        add: function add(node, className) {
+            node.classList.add(className);
+        },
+        remove: function remove(node, className) {
+            node.classList.remove(className);
+        },
+        has: function has(node, className) {
+            //判断node节点的指定类名是否存在
+            return node.classList.contains(className); //存在返回true，不存在返回false
+        }
+    },
+    //添加事件
+    on: function on(node, eventName, fn) {
+        node.addEventListener(eventName, fn);
+    },
+
+    //删除事件
+    off: function off(node, eventName, fn) {
+        node.removeEventListener(eventName, fn);
+    },
+
+    //查找或获取标签
+    find: function find(selector, scope) {
+        return document.querySelectorAll(selector);
+    },
+
+    //查找父元素
+    parent: function parent(node) {
+        return node.parentNode;
+    },
+
+    //查找子元素
+    children: function children(node) {
+        return node.children;
+    },
+
+    //查找兄弟元素
+    siblings: function siblings(node) {
+        //查找元素的父元素下的所有子元素，再把元素本身过滤出来即可得到剩余的兄弟元素
+        var arr = Array.from(node.parentNode.children).filter(function (n) {
+            return n !== node;
+        });
+        return arr;
+    },
+
+    //获取下一个节点
+    next: function next(node) {
+        var x = node.nextSibling;
+        while (x && x.nodeType === 3) {
+            x = x.nextSibling;
+        }
+        return x;
+    },
+
+    //获取上一个节点元素
+    previous: function previous(node) {
+        var x = node.previousSibling;
+        while (x && x.nodeType === 3) {
+            x = x.previousSibling;
+        }
+        return x;
+    },
+
+    //遍历所有节点
+    each: function each(nodeList, fn) {
+        for (var i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i]);
+        }
+    },
+
+    //获取元素下标
+    index: function index(node) {
+        var list = dom.children(node.parentNode);
+        var i = void 0;
+        for (i = 0; i < list.length; i++) {
+            if (list[i] === node) {
+                break;
+            }
+        }
+        return i;
+    }
+};
+},{}],"../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57898' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+
+      removeErrorOverlay();
+
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["../../../.config/yarn/global/node_modules/parcel/src/builtins/hmr-runtime.js","dom.js"], null)
+//# sourceMappingURL=/dom.be574493.map
