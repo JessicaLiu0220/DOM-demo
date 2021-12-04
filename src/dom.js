059abffbf0494b529478ad0fab4a7a03442e0c61
Jessica <1330784336@qq.com>
2021-12-04 15:30:45 +0800

    DOM库及用例

--- src/dom.js
+++ src/dom.js
@@ -1,81 +1,98 @@
 window.dom = {
+    //创建元素：首先定义一个container容器放创建的新标签，template可以放入任意元素，
+    //利用innerHTML将容器中的内容变成string，trim（）的作用是去掉字符串两侧的空格
+    //最后返回firstChild。
     create(string) {
-        let contain = document.createElement("template")
-        contain.innerHTML = string.trim();
-        return contain.content.firstChild;
+        const container = document.createElement("template")
+        container.innerHTML = string.trim();
+        return container.content.firstChild;
     },
-    after(node, node2) {
-        node.parentNode.insertBefore(node2, node.nextSibling);
+    // 插入后一个兄弟元素：插入node下一个节点的前面，相当于插入node的后面
+    after(node, didi) {
+        node.parentNode.insertBefore(didi, node.nextSiling);
     },
-    before(node, node2) {
-        node.parentNode.insertBefore(node2, node);
-
+    //插入一个哥哥元素
+    before(node, gege) {
+        node.parentNode.insertBefore(gege, node)
     },
+    //新增一个子元素
     append(parent, node) {
         parent.appendChild(node);
     },
+    //新增一个父元素：把父元素插入到子元素之前，再将子元素插入到父元素中，原来的子元素的位置就会移开
     wrap(node, parent) {
-        dom.before(node, parent);
-        dom.append(parent, node);
+        dom.before(node, parent)
+        dom.append(parent, node)
+
     },
+    //删除元素
     remove(node) {
         node.parentNode.removeChild(node);
         return node;
     },
+    //删除所有子元素:用while判断node的第一个子节点是否存在，存在就执行删除操作，并把删除的值放到数组中
+    //当x不存在即node子元素删除完结束循环，返还被删除的元素的数组。
     empty(node) {
         const childNodes = node.childNodes;
-        const arr = [];
+        const array = [];
         let x = node.firstChild;
         while (x) {
-            arr.push(dom.remove(node.firstChild));
-            x = node.firstChild;
+            array.push(dom.remove(x));
+            x = node.firstChild
         }
-        return arr;
+        return array;
     },
+    //修改或查看元素的属性值：当参数的长度为3时，为修改node属性值
+    //当参数的长度为2时，为查看node中的某一属性值
     attr(node, name, value) {
         if (arguments.length === 3) {
-            node.setAttribute(name, value);
+            node.setAttribute(name, value)
         } else if (arguments.length === 2) {
-            return node.getAttribute(name);
+            return node.setAttribute(name)
+        }
+    },
+    //读写文本内容
+    text(node, string) {
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string;
+            } else {
+                node.textContent = string;
+            }
+        } else if (arguments.length === 1) {
+            if ('innerText' in node) {
+                return node.innerText;
+            } else {
+                return node.textContent;
+            }
         }
 
     },
-    text(node, content) {//适配
-        if ('innerText' in node) {
-            node.innerText = content;
-        } else {
-            node.textContent = content;
-        }
-
-    },
-    //改html的内容
+    //读写html内容
     html(node, string) {
         if (arguments.length === 2) {
             node.innerHTML = string;
         } else if (arguments.length === 1) {
             return node.innerHTML;
         }
-
     },
-    //修改样式
+    //读取或修改style属性
     style(node, name, value) {
+        //style(text,'color','red'),当长度为3时修改name的value值
         if (arguments.length === 3) {
-            //dom.style(test,'color','red')
             node.style[name] = value;
         } else if (arguments.length === 2) {
-            //判断name的类型是否为字符串
             if (typeof name === 'string') {
-                //dom.style(test,'color')是字符串就返回其属性
+                //style(test,'color')，长度为2且name类型为string时查看name属性值
                 return node.style[name];
-
-            } else if (name instanceof Object) {//判断name的类型是不是对象
-                const object = name;
-                for (let key in object) {//修改对象和元素都具有的属性key的值
-                    node.style[key] = object[key];
-                }
+            } else if (name instanceof Object) {
+                //style(test,{color:'red'})长度为2且name为对象时修改name的style
+                for (let key in name)
+                    node.style[key] = name[key]
             }
         }
     },
+    //添加class
     class: {
         add(node, className) {
             node.classList.add(className)
@@ -83,60 +100,67 @@ window.dom = {
         remove(node, className) {
             node.classList.remove(className)
         },
-        has(node, className) {
-            return node.classList.contains(className)
+        has(node, className) {//判断node节点的指定类名是否存在
+            return node.classList.contains(className)//存在返回true，不存在返回false
         }
     },
-    on(node, event, fn) {//添加事件
-        node.addEventListener(event, fn);
+    //添加事件
+    on(node, eventName, fn) {
+        node.addEventListener(eventName, fn)
     },
-    off(node, event, fn) {//删除事件
-        node.removeEventListener(event, fn);
+    //删除事件
+    off(node, eventName, fn) {
+        node.removeEventListener(eventName, fn)
     },
+    //查找或获取标签
     find(selector, scope) {
-        return (scope || document).querySelectorAll(selector);  //如果有范围就在范围里查找元素，如果没有就在document中查找
+        return document.querySelectorAll(selector);
     },
+    //查找父元素
     parent(node) {
-        return node.parentNode
+        return node.parentNode;
     },
+    //查找子元素
     children(node) {
         return node.children;
     },
-    //获取兄弟节点
-    siblings(node) {//先到父元素然后找到所有的子节点，再变成数组将自己过滤出去
-        return Array.from(node.parentNode.children).filter(n => n !== node)
+    //查找兄弟元素
+    siblings(node) {//查找元素的父元素下的所有子元素，再把元素本身过滤出来即可得到剩余的兄弟元素
+        const arr = Array.from(node.parentNode.children).filter(n => n !== node);
+        return arr;
     },
-    //查找下一个节点
+    //获取下一个节点
     next(node) {
-        let x = node.nextSibling;
+        let x = node.nextSibling
         while (x && x.nodeType === 3) {
-            x = x.nextSibling  //当x是文本时x为下一个兄弟元素
+            x = x.nextSibling
         }
-        return x;
+        return x
     },
-    //查找上一个节点
+    //获取上一个节点元素
     previous(node) {
-        let x = node.previousSibling;
+        let x = node.previousSibling
         while (x && x.nodeType === 3) {
-            x = x.previousSibling  //当x是文本时x为下一个兄弟元素
+            x = x.previousSibling
         }
-        return x;
+        return x
     },
-    //遍历每个元素
-    each(nodelist, fn) {
-        for (let i = 0; i < nodelist.length; i++) {
-            fn.call(null, nodelist[i])
+    //遍历所有节点
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
         }
     },
-    //查看元素的下标
+    //获取元素下标
     index(node) {
-        let i;
-        const list = dom.children(node.parentNode);
+        const list = dom.children(node.parentNode)
+        let i
         for (i = 0; i < list.length; i++) {
             if (list[i] === node) {
                 break
             }
         }
-        return i;
+        return i
     }
-};
+
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,56 @@
-const div = dom.find('#test>.red')[0]  // 获取对应的元素
-console.log(div);
-dom.style(div, 'color', 'red')  // 设置 div.style.color
-const divList = dom.find('.red')  // 获取多个 div.red 元素
-console.log(divList)
-dom.each(divList, (n) => console.log(n))  // 遍历 divList 里的所有元素
+//create
+const create1 = dom.create("<div id='create1'><span>create封装</span></div>");
+console.log(create1);
+//after
+console.log(dom.after(test, create1));
+//before
+const create2 = dom.create("<div id='create2'>create2</div>")
+dom.before(test, create2)
+//append
+const node1 = dom.create("<div id='node1'>子元素</div>")
+dom.append(test, node1)
+//wrap
+const node2 = dom.create("<div id='node2'></div>")
+dom.wrap(test, node2)
+//remove
+dom.remove(node1)
+//empty
+const node3 = window.empty;
+console.log(dom.empty(node3))
+//attr
+dom.attr(create2, 'id', 'attr')
+//text
+dom.text(test, '你好，这是一段新的文本')
+console.log(dom.text(test))
+//html
+dom.text(test, '文本又更新了')
+console.log(dom.text(test))
+//style
+dom.style(test, 'color', 'red')
+console.log(dom.style(test, 'color'))
+dom.style(test, { color: 'yellow' })
+//class
+dom.class.add(test, 'red')
+dom.class.remove(test, 'red')
+console.log(dom.class.has(test, 'red'))
+//on和off
+let fn = () => {
+    console.log('点击了')
+}
+dom.on(test, 'click', fn)
+dom.off(test, 'click', fn)
+//find
+console.log(dom.find('#attr')[0]);
+//parent
+console.log(dom.parent(test))
+//children
+console.log(dom.children(node2)[0])
+//siblings
+const s2 = dom.find('#s2')[0]
+console.log(dom.siblings(s2))
+//next
+console.log(dom.next(s2))
+//previous
+console.log(dom.previous(s2))
+//index
+console.log(dom.index(s2))
